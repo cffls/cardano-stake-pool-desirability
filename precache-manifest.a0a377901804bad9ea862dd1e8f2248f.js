0259f6f75b5c6ef1a7c4076317af7a9e7d9b3f09
Fan Chen <cffls@hotmail.com>
2019-03-01 23:29:37 -0800

    Updates

---
+++ precache-manifest.a0a377901804bad9ea862dd1e8f2248f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6adc28c4da00c4a40c16",
+    "url": "/cardano-stake-pool-desirability/static/js/runtime~main.6adc28c4.js"
+  },
+  {
+    "revision": "eecd34f21e0917fa68a5",
+    "url": "/cardano-stake-pool-desirability/static/js/main.eecd34f2.chunk.js"
+  },
+  {
+    "revision": "b97c76ec2a18c0eae63a",
+    "url": "/cardano-stake-pool-desirability/static/js/2.b97c76ec.chunk.js"
+  },
+  {
+    "revision": "eecd34f21e0917fa68a5",
+    "url": "/cardano-stake-pool-desirability/static/css/main.699ac8f0.chunk.css"
+  },
+  {
+    "revision": "d7103131e60d13768f218d9c2ff4d03c",
+    "url": "/cardano-stake-pool-desirability/index.html"
+  }
+];
